f0d537309a48c5927550b884b1a628a73b77ae6d
Roksolana Veres <Roxolanka1@ukr.net>
2025-06-05 16:46:56 +0300

    draft form input handling & avatar preview

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,33 +3,75 @@ import infoIcon from "../assets/images/icon-info.svg";
 import { useState } from "react";
 
 export default function Form() {
-  const [avatar, setAvatar] = useState(null);
-  const [avatarError, setAvatarError] = useState(null);
+  // const [avatarError, setAvatarError] = useState(null);
+  const [userInfo, setUserInfo] = useState({
+    fullName: "",
+    email: "",
+    username: "",
+    avatarFile: null,
+    avatarPreview: null,
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    fullNameError: null,
+    emailError: null,
+    usernameError: null,
+    avatarError: null,
+  });
+
+  function handleInputChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    if (value === "") {
+      setFormErrors((prev) => ({
+        ...prev,
+        [name + "Error"]: `This is a required field. Please enter your ${name}`,
+      }));
+    } else {
+      setUserInfo((prevInfo) => ({
+        ...prevInfo,
+        [name]: value,
+      }));
+      setFormErrors((prev) => ({
+        ...prev,
+        [name + "Error"]: null,
+      }));
+    }
+  }
 
   function handleFileChange(e) {
     const file = e.target.files[0];
+    if (!file) return;
 
-    if (file) {
-      const isUnder500KB = file.size <= 500 * 1024;
-      if (!isUnder500KB) {
-        setAvatarError("File must be less than 500KB");
-        return;
-      }
-
-      // preview image
-
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setAvatar(reader.result);
-        setAvatarError(null);
-      };
-      reader.readAsDataURL(file);
+    if (file.size > 500 * 1024) {
+      setFormErrors((prev) => ({
+        ...prev,
+        avatarError: "File must be less than 500KB",
+      }));
+      return;
     }
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setUserInfo((prev) => ({
+        ...prev,
+        avatarFile: file,
+        avatarPreview: reader.result,
+      }));
+      setFormErrors((prev) => ({
+        ...prev,
+        avatarError: null,
+      }));
+    };
+
+    reader.readAsDataURL(file);
   }
+
   return (
     <form>
       <div className="form-field">
-        <label for="avatar">
+        <label htmlFor="avatar">
           <p className="label-text">Upload Avatar</p>
           <div className="file-input-container">
             <input
@@ -39,11 +81,11 @@ export default function Form() {
               accept="image/jpg, image/png, image/jpeg"
               onChange={handleFileChange}
             />
-            {avatar ? (
-              <img src={avatar} alt="avatar preview" className="avatarPreview" />
+            {userInfo.avatarPreview ? (
+              <img src={userInfo.avatarPreview} alt="avatar preview" className="avatarPreview" />
             ) : (
               <div className="uploadIcon-container">
-                <img src={uploadIcon} />
+                <img src={uploadIcon} alt="upload icon" />
               </div>
             )}
 
@@ -51,8 +93,8 @@ export default function Form() {
           </div>
         </label>
         <div className="input-notification">
-          <img src={infoIcon} />
-          {avatarError ? (
+          <img src={infoIcon} alt="info icon" />
+          {formErrors.avatarError ? (
             <p className="input-error">File too large. Please upload a photo under 500KB</p>
           ) : (
             <p>Upload your photo (JPG or PNG, max size: 500KB)</p>
@@ -61,17 +103,25 @@ export default function Form() {
       </div>
 
       <div className="form-field">
-        <label for="name" className="label-text">
+        <label htmlFor="fullName" className="label-text">
           Full Name
         </label>
-        <input type="text" id="name" name="name" className="inputText" />
-        <div className="input-notification hidden">
-          <img src={infoIcon} />
-          <p className="input-error">Please enter your name</p>
-        </div>
+        <input
+          type="text"
+          id="fullName"
+          name="fullName"
+          className="inputText"
+          onChange={handleInputChange}
+        />
+        {formErrors.fullNameError && (
+          <div className="input-notification ">
+            <img src={infoIcon} alt="info icon" />
+            <p className="input-error">{formErrors.fullNameError}</p>
+          </div>
+        )}
       </div>
       <div className="form-field">
-        <label for="email" className="label-text">
+        <label htmlFor="email" className="label-text">
           Email Address
         </label>
         <input
@@ -80,14 +130,17 @@ export default function Form() {
           name="email"
           placeholder="example@email.com"
           className="inputText"
+          onChange={handleInputChange}
         />
-        <div className="input-notification hidden">
-          <img src={infoIcon} />
-          <p className="input-error">Please enter a valid email address</p>
-        </div>
+        {formErrors.emailError && (
+          <div className="input-notification ">
+            <img src={infoIcon} alt="info icon" />
+            <p className="input-error">{formErrors.emailError}</p>
+          </div>
+        )}
       </div>
       <div className="form-field">
-        <label for="username" className="label-text">
+        <label htmlFor="username" className="label-text">
           GitHub Username
         </label>
         <input
@@ -96,11 +149,14 @@ export default function Form() {
           name="username"
           placeholder="@yourusername"
           className="inputText"
+          onChange={handleInputChange}
         />
-        <div className="input-notification hidden">
-          <img src={infoIcon} />
-          <p className="input-error">Please enter your username</p>
-        </div>
+        {formErrors.usernameError && (
+          <div className="input-notification ">
+            <img src={infoIcon} alt="info icon" />
+            <p className="input-error">{formErrors.usernameError}</p>
+          </div>
+        )}
       </div>
       <div className="form-field">
         <button type="submit">Generate My Ticket</button>
